467d6c7ccc45a9601c966f246ada76d1c3763afd
Drew-Watson-117 <drewzidrew1@gmail.com>
2023-11-08 11:20:09 -0700

    Done implementing, time to test

--- main.c
+++ main.c
@@ -1,121 +1,201 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mpi.h>
 
-void printArray(int* a, int size);
-void printArray(float* a, int size);
+void printIntArray(int* a, int size);
+void printFloatArray(float* a, int size);
+void serialHistogram(float* data,int binCount, float minMeas, float maxMeas, int dataCount);
 
 int main(int argc, char const* argv[])
-{
-    if (argc == 5)
-    {
-        
-        int myRank;
-        int threadCount;
-        int binCount = atoi(argv[1]);
-        float minMeas = atof(argv[2]);
-        float maxMeas = atof(argv[3]);
-        int dataCount = atoi(argv[4]);
-        float* localData;
-        int localDataCount;
-        float* binMaxes = (float*)malloc(binCount*sizeof(float));
-        int* globalBinCounts = (int*)malloc(binCount*sizeof(int));
-
-        // Find bin maxes
-        for (int i = 0; i < binCount; i++) {
-            binMaxes[i] = minMeas + i * (maxMeas - minMeas) / binCount;
-        }
-
-        srand(100);
-
-        float* data = (int*)malloc(dataCount*sizeof(float));
-        // Load data with random float values
-        for (int i = 0; i < dataCount; i++)
-        {
-            float randomFloat = (maxMeas - minMeas) * (float)(rand()) / (float)(RAND_MAX)+minMeas; //Guarantees randomFloat to be in [minMeas,maxMeas]
-            data[i] = randomFloat;
-        }   
+{       
+        int myRank, threadCount, localDataCount, binCount, dataCount;
+        float minMeas, maxMeas;
+        float* binMaxes;
+        int* globalBinCounts;
+        float *localData = NULL;
+        float *data = NULL;
+        int* sendCounts = NULL;
+        int* displs = NULL;
 
         MPI_Init(NULL,NULL);
         MPI_Comm_size(MPI_COMM_WORLD, &threadCount);
-        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);    
         
-
         if (myRank == 0) {
+            if (argc == 5) {
+                binCount = atoi(argv[1]);
+                minMeas = atof(argv[2]);
+                maxMeas = atof(argv[3]);
+                dataCount = atoi(argv[4]);
 
-            // Send sub arrays of data to different threads
-            int* sendCounts = (int*)malloc(threadCount*sizeof(int));
+                globalBinCounts = malloc(binCount*sizeof(int));
+                for (int i = 0; i < binCount; i++) {
+                    globalBinCounts[i] = 0;
+                }
 
-            for (int rank = 0; rank < threadCount; rank++)
-            {
-                // Make sub array
-                int localCount;
-                if (rank < dataCount % threadCount) {
-                    // size is dataCount / threadCount + 1
-                    localCount = dataCount / threadCount + 1;
+                sendCounts = malloc(threadCount*sizeof(int));
+                binMaxes = (float*)malloc(binCount*sizeof(float));
+
+                // Find bin maxes
+                for (int i = 0; i < binCount; i++) {
+                    binMaxes[i] = minMeas + (i+1) * (maxMeas - minMeas) / binCount;
                 }
-                else {
-                    // size is dataCount / threadCount
-                    localCount = dataCount / threadCount;
+
+                // Generate Random data
+                srand(100);
+                float* data = (float*)malloc(dataCount*sizeof(float));
+                // Load data with random float values
+                for (int i = 0; i < dataCount; i++)
+                {
+                    float randomFloat = (maxMeas - minMeas) * (float)(rand()) / (float)(RAND_MAX)+minMeas; //Guarantees randomFloat to be in [minMeas,maxMeas]
+                    data[i] = randomFloat;
                 }
-                sendCounts[rank] = localCount;
-            } 
-            // Send localDataCount
-            MPI_Scatter(sendCounts, 1, MPI_INT, &localDataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            // Send localData
-            MPI_Scatterv(data,sendCounts,0,MPI_FLOAT,localData,sendCounts[myRank],MPI_FLOAT,0,MPI_COMM_WORLD); 
-            // Set own localDataCount and localData
-            memcpy(localData,data,sendCounts[0]*sizeof(float));
-            localDataCount = sendCounts[0];
+                // Broadcast collected data and bin maxes to other threads
+                MPI_Bcast(&binCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+                MPI_Bcast(&minMeas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+                MPI_Bcast(&maxMeas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+                MPI_Bcast(&dataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+                MPI_Bcast(binMaxes, binCount, MPI_FLOAT, 0, MPI_COMM_WORLD);
+                MPI_Bcast(globalBinCounts,binCount,MPI_INT,0,MPI_COMM_WORLD);
+
+                // Send sub arrays of data to different threads
+                int* displs = malloc(threadCount*sizeof(int));
+
+                for (int rank = 0; rank < threadCount; rank++)
+                {
+                    // Make sub array
+                    int localCount;
+                    if (rank < dataCount % threadCount) {
+                        // size is dataCount / threadCount + 1
+                        localCount = dataCount / threadCount + 1;
+                    }
+                    else {
+                        // size is dataCount / threadCount
+                        localCount = dataCount / threadCount;
+                    }
+                    sendCounts[rank] = localCount;
+                    if (rank == 0) {
+                        displs[0] = 0;
+                    }
+                    else {
+                        displs[rank] = displs[rank-1] + sendCounts[rank-1];
+                    }
+                } 
+                // Send localDataCount
+                MPI_Scatter(sendCounts, 1, MPI_INT, &localDataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+                // Send localData
+                // MPI_Scatterv(data,sendCounts,displs,MPI_FLOAT,localData,sendCounts[myRank],MPI_FLOAT,0,MPI_COMM_WORLD); // Segmentation Fault due to sendCounts[myRank]
+                MPI_Scatterv(data,sendCounts,displs,MPI_FLOAT,localData,0,MPI_FLOAT,0,MPI_COMM_WORLD);
+                // Set own localDataCount and localData
+                localDataCount = sendCounts[0];
+                localData = malloc(localDataCount*sizeof(float));
+                memcpy(localData,data,localDataCount*sizeof(float));
+                printf("Data: ");
+                printFloatArray(data,dataCount);
+            }
+            else {
+                printf("Error: Invalid number of command line arguments specified");
+                return 1;
+            }  
         }
         else {
             // Collect localDataCount and localData from rank 0
-            MPI_Scatter(NULL, NULL, MPI_INT, &localDataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            localData = (float*)malloc(localDataCount*sizeof(float));
-            MPI_Scatterv(NULL, NULL, NULL, MPI_FLOAT, localData, localDataCount, MPI_FLOAT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&binCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&minMeas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&maxMeas, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&dataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            binMaxes = malloc(binCount*sizeof(float));
+            globalBinCounts = malloc(binCount*sizeof(float));
+            MPI_Bcast(binMaxes, binCount, MPI_FLOAT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(globalBinCounts,binCount,MPI_INT,0,MPI_COMM_WORLD);
+
+            MPI_Scatter(sendCounts, 1, MPI_INT, &localDataCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            localData = malloc(localDataCount*sizeof(float));
+            MPI_Scatterv(data,sendCounts,displs,MPI_FLOAT,localData,localDataCount,MPI_FLOAT,0,MPI_COMM_WORLD);
         }
         
         // Compute local histogram
-
-        //int localBinCounts[binCount];
-        int* localBinCounts = (int*)malloc(binCount*sizeof(int));
+        int* localBinCounts = malloc(binCount*sizeof(int));
+        for (int i = 0; i < binCount; i++) {
+            localBinCounts[i] = 0;
+        }
         // Bin the data
         for (int i = 0; i < localDataCount; i++) {
             float dataPoint = localData[i];
-            for (int k = 0; k < binCount; k++) {
-                if (dataPoint < binMaxes[k]) {
+            int k = 0;
+            int flag = 0;
+            while (k < binCount && flag == 0) {
+                if (dataPoint <= binMaxes[k]) {
                     localBinCounts[k]++;
-                    break;
+                    flag = 1;
                 }
+                k++;
             }
         }
+
+        // Wait for all local sums to finish
+        MPI_Barrier(MPI_COMM_WORLD);
+
         // Use MPI_Reduce to combine sums
-        for (int i = 0; i < binCount; i++) {
-            MPI_Reduce(&(localBinCounts[i]), &(globalBinCounts[i]), 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        MPI_Reduce(localBinCounts,globalBinCounts,binCount,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+        if (myRank == 0)
+        {
+            // Rank 0 prints the results
+            printf("Bin Maxes: ");
+            printFloatArray(binMaxes, binCount);
+            printf("Bin Counts: ");
+            printIntArray(globalBinCounts, binCount);
+            // serialHistogram(data,binCount,minMeas,maxMeas,dataCount);
         }
 
-        MPI_Finalize();
+        free(binMaxes), free(localData), free(sendCounts);
+        free(localBinCounts);
+        free(data);
 
-        // Print results serially
-        printf("Bin Maxes: ");
-        printArray(binMaxes, binCount);
-        printf("Bin Counts: ");
-        printArray(globalBinCounts, binCount);
-    }
+        MPI_Finalize();  
 
     return 0;
 }
 
-void printArray(int* a, int size) {
+void printIntArray(int* a, int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d , ",a[i]);
-    }
-    printf("\n");
-}
-void printArray(float* a, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d , ",a[i]);
+        printf("%d ",a[i]);
     }
     printf("\n");
 }
 
+void printFloatArray(float* a, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%f ",a[i]);
+    }
+    printf("\n");
+}
+
+void serialHistogram(float* data,int binCount, float minMeas, float maxMeas, int dataCount) {
+    float* binMaxes = malloc(binCount*sizeof(float));
+    int* binCounts = malloc(binCount*sizeof(int));
+    for (int i = 0; i < binCount; i++) {
+        binMaxes[i] = minMeas + (i+1) * (maxMeas - minMeas) / binCount;
+        binCounts[i] = 0;
+    }
+    for (int i = 0; i < dataCount; i++) {
+        float dataPoint = data[i];
+        int k = 0;
+        int flag = 0;
+        while (k < binCount && flag == 0) {
+            if (dataPoint <= binMaxes[k]) {
+                binCounts[k]++;
+                flag = 1;
+            }
+            k++;
+        }
+    }
+    printf("===== SERIAL HISTOGRAM =====\n");
+    printf("Bin Maxes: ");
+    printFloatArray(binMaxes, binCount);
+    printf("Bin Counts: ");
+    printIntArray(binCounts, binCount);
+}
